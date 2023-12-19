5d80b04631372d2b1b766ff888d1916f6e9a4908
Waquar <mdwaquarazam1234@gmail.com>
2023-12-19 23:25:12 +0530

    created superuser,add data/post create,admin.py list-display

--- blog/App/admin.py
+++ blog/App/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Article
 # Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    list_display=('title','content','publication_date')
+          
+    
+admin.site.register(Article,ArticleAdmin)
