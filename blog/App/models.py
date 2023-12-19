ed1b3a7748da4efb0ddce06b64e35451cfe65a16
Waquar <mdwaquarazam1234@gmail.com>
2023-12-19 23:12:31 +0530

    database model created

--- blog/App/models.py
+++ blog/App/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    publication_date = models.DateField()
+    
+    def __str__(self):
+        return self.title
+ 
