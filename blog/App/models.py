d23574ded4a330b8f8ab853f8eb46501b103d522
Waquar <mdwaquarazam1234@gmail.com>
2023-12-21 11:33:38 +0530

    placeholder in title,content

--- blog/App/models.py
+++ blog/App/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Article(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
+    title = models.CharField(max_length=200,help_text='Enter the title.')
+    content = models.TextField(help_text='Enter the content here.')
     publication_date = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
